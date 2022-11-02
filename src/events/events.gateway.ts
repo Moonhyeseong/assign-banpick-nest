bed08ef88862a4a807ad91f01c8ca69f43c6f5c1
Moonhyeseong <moonhs97@gmail.com>
2022-11-02 14:58:09 +0900

    error throw 수정

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -66,6 +66,8 @@ export class EventsGateway {
     const gameId = data.gameId;
     const userId = data.userId;
 
+    if (gameId === undefined) throw new BadRequestException('missing gameId');
+
     //단일 게임 데이터 업데이트
     this.gameModel.findById({ _id: gameId }, (err: object, result: IGame) => {
       if (err) throw new BadRequestException(err);
